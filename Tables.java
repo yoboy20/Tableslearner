433bf7f945590e3cb39a8ca5d1f7759bb463b110
yoboy20 <arnavasthana21@gmail.com>
2021-03-14 12:50:08 +0530

    Initial sharing of project

---
+++ Tables.java
@@ -0,0 +1,10 @@
+// Program to print multiplication Tables 
+public class Tables{
+  public static void main(int a){
+     for(int i=1;i<=a;i++){
+        for(int j=1;j<=10;j++)
+           System.out.println(i+"*"+j+"="+(i*j)+"\t");
+       System.out.println();    
+        }
+     }
+    }
